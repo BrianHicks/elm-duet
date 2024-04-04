63e8db9d9a9f15d88b3fc34b7e3748846f0449d8
Brian Hicks <brian@brianthicks.com>
2024-04-04 05:24:24 -0500

    hook up basic error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+use eyre::Result;
+
+#[derive(Debug, Parser)]
+struct Cli {}
+
+impl Cli {
+    fn run(&self) -> Result<()> {
+        println!("{self:#?}");
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if let Err(problem) = cli.run() {
+        println!("{problem:?}");
+        std::process::exit(1);
+    }
 }
