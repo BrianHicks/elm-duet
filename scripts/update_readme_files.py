71ab9a395d419be91477f1a6a5c203de12896b61
Brian Hicks <brian@brianthicks.com>
2024-04-12 06:22:55 -0500

    keep examples in README updated

---
+++ scripts/update_readme_files.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import re
+
+with open('README.md', 'r') as fh:
+    orig = fh.read()
+
+matches = list(re.finditer(r'(?P<open>```\w+\s*\{source=(?P<source>.+?)\}?\n)(.*?)```', orig, re.DOTALL))
+matches.reverse() # iterate in reverse order to not mess up indexes while we edit
+
+edited = orig
+
+for match in matches:
+    start, end = match.span()
+    parts = match.groupdict()
+
+    with open(parts['source'], 'r') as fh:
+        file_contents = fh.read()
+
+    edited = edited[:start] + parts["open"] + file_contents + "\n```" + edited[end:]
+
+with open('README.md', 'w') as fh:
+    fh.write(edited)
