3b585d88a7f673953cfd923e1e6a93f9eaa1b3f3
Brian Hicks <brian@brianthicks.com>
2024-04-19 22:57:08 -0500

    comment out unused code for now

--- src/elm.rs
+++ src/elm.rs
@@ -151,23 +151,23 @@ impl Type {
     }
 }
 
-pub struct Module {
-    name: Vec<String>,
-    defs: Vec<()>,
-}
-
-impl Module {
-    pub fn to_source(&self) -> Result<String> {
-        if self.defs.is_empty() {
-            eyre::bail!(
-                "Module {} didn't contain any definitions",
-                self.name.join(".")
-            )
-        }
-
-        Ok(String::new())
-    }
-}
+// pub struct Module {
+//     name: Vec<String>,
+//     defs: Vec<()>,
+// }
+//
+// impl Module {
+//     pub fn to_source(&self) -> Result<String> {
+//         if self.defs.is_empty() {
+//             eyre::bail!(
+//                 "Module {} didn't contain any definitions",
+//                 self.name.join(".")
+//             )
+//         }
+//
+//         Ok(String::new())
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -418,22 +418,22 @@ mod tests {
         }
     }
 
-    mod module {
-        use super::*;
-
-        #[test]
-        fn error_on_no_defs_to_source() {
-            let m = Module {
-                name: Vec::from(["A".to_string(), "B".to_string()]),
-                defs: Vec::new(),
-            };
-
-            let err = m.to_source().unwrap_err();
-
-            assert_eq!(
-                err.to_string(),
-                "Module A.B didn't contain any definitions".to_string()
-            )
-        }
-    }
+    // mod module {
+    //     use super::*;
+    //
+    //     #[test]
+    //     fn error_on_no_defs_to_source() {
+    //         let m = Module {
+    //             name: Vec::from(["A".to_string(), "B".to_string()]),
+    //             defs: Vec::new(),
+    //         };
+    //
+    //         let err = m.to_source().unwrap_err();
+    //
+    //         assert_eq!(
+    //             err.to_string(),
+    //             "Module A.B didn't contain any definitions".to_string()
+    //         )
+    //     }
+    // }
 }
