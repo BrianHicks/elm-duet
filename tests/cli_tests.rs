95440f548c5cae845982b3651adff03a14e60694
Brian Hicks <brian@brianthicks.com>
2024-04-30 06:17:01 -0500

    add node_modules/.bin to tests

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -1,6 +1,18 @@
 #[test]
 fn cli_tests() {
     trycmd::TestCases::new()
+        .env(
+            "PATH",
+            format!(
+                "{}:{}",
+                std::env::current_dir()
+                    .expect("a current dir should exist")
+                    .join("node_modules")
+                    .join(".bin")
+                    .display(),
+                std::env!("PATH")
+            ),
+        )
         .case("tests/cmd/*.toml")
         .case("README.md");
 }
