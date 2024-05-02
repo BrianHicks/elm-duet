bcc396f4ac4f35362ff020cc4f61682ec8be3770
Brian Hicks <brian@brianthicks.com>
2024-05-02 06:53:24 -0500

    update with latest tinyping schema

--- tests/cmd/tinyping.out/src/Main/Ports.elm
+++ tests/cmd/tinyping.out/src/Main/Ports.elm
@@ -129,40 +129,40 @@ encodeChangeDocument changeDocument_ =
     Json.Encode.list (/value -> encodeChangeDocumentElements value) changeDocument_
 
 
-type alias PingV1 =
+type alias Ping =
     { custom : Dict String String
     , tag : Maybe String
     , time : Int
     }
 
 
-pingV1Decoder : Json.Decode.Decoder PingV1
-pingV1Decoder =
-    Json.Decode.succeed PingV1
+pingDecoder : Json.Decode.Decoder Ping
+pingDecoder =
+    Json.Decode.succeed Ping
         |> Json.Decode.Pipeline.required "custom" (Json.Decode.dict Json.Decode.string)
         |> Json.Decode.Pipeline.required "tag" (Json.Decode.nullable Json.Decode.string)
         |> Json.Decode.Pipeline.required "time" Json.Decode.int
 
 
-encodePingV1 : PingV1 -> Json.Encode.Value
-encodePingV1 pingV1_ =
+encodePing : Ping -> Json.Encode.Value
+encodePing ping_ =
     Json.Encode.object
-        [ ( "custom", Json.Encode.dict identity (/value -> Json.Encode.string value) pingV1_.custom )
+        [ ( "custom", Json.Encode.dict identity (/value -> Json.Encode.string value) ping_.custom )
         , ( "tag"
-          , case pingV1_.tag of
+          , case ping_.tag of
                 Just value ->
                     Json.Encode.string value
 
                 Nothing ->
                     Json.Encode.null
           )
-        , ( "time", Json.Encode.int pingV1_.time )
+        , ( "time", Json.Encode.int ping_.time )
         , ( "version", Json.Encode.string "v1" )
         ]
 
 
 type PingsElements
-    = VersionedPingsElementsV1 PingV1
+    = VersionedPingsElementsV1 Ping
 
 
 pingsElementsDecoder : Json.Decode.Decoder PingsElements
@@ -171,7 +171,7 @@ pingsElementsDecoder =
         (/tag ->
             case tag of
                 "v1" ->
-                    Json.Decode.map VersionedPingsElementsV1 pingV1Decoder
+                    Json.Decode.map VersionedPingsElementsV1 pingDecoder
 
                 unknown ->
                     Json.Decode.fail ("Unknown value `" ++ unknown ++ "`")
@@ -183,39 +183,39 @@ encodePingsElements : PingsElements -> Json.Encode.Value
 encodePingsElements pingsElements_ =
     case pingsElements_ of
         VersionedPingsElementsV1 versionedPingsElementsV1 ->
-            encodePingV1 versionedPingsElementsV1
+            encodePing versionedPingsElementsV1
 
 
-type alias SettingsV1 =
+type alias Settings =
     { minutesPerPing : Int
     }
 
 
-settingsV1Decoder : Json.Decode.Decoder SettingsV1
-settingsV1Decoder =
-    Json.Decode.succeed SettingsV1
+settingsDecoder : Json.Decode.Decoder Settings
+settingsDecoder =
+    Json.Decode.succeed Settings
         |> Json.Decode.Pipeline.required "minutesPerPing" Json.Decode.int
 
 
-encodeSettingsV1 : SettingsV1 -> Json.Encode.Value
-encodeSettingsV1 settingsV1_ =
+encodeSettings : Settings -> Json.Encode.Value
+encodeSettings settings_ =
     Json.Encode.object
-        [ ( "minutesPerPing", Json.Encode.int settingsV1_.minutesPerPing )
+        [ ( "minutesPerPing", Json.Encode.int settings_.minutesPerPing )
         , ( "version", Json.Encode.string "v1" )
         ]
 
 
-type Settings
-    = VersionedSettingsV1 SettingsV1
+type DocumentSettings
+    = VersionedDocumentSettingsV1 Settings
 
 
-settingsDecoder : Json.Decode.Decoder Settings
-settingsDecoder =
+documentSettingsDecoder : Json.Decode.Decoder DocumentSettings
+documentSettingsDecoder =
     Json.Decode.andThen
         (/tag ->
             case tag of
                 "v1" ->
-                    Json.Decode.map VersionedSettingsV1 settingsV1Decoder
+                    Json.Decode.map VersionedDocumentSettingsV1 settingsDecoder
 
                 unknown ->
                     Json.Decode.fail ("Unknown value `" ++ unknown ++ "`")
@@ -223,37 +223,37 @@ settingsDecoder =
         (Json.Decode.field "version" Json.Decode.string)
 
 
-encodeSettings : Settings -> Json.Encode.Value
-encodeSettings settings_ =
-    case settings_ of
-        VersionedSettingsV1 versionedSettingsV1 ->
-            encodeSettingsV1 versionedSettingsV1
+encodeDocumentSettings : DocumentSettings -> Json.Encode.Value
+encodeDocumentSettings documentSettings_ =
+    case documentSettings_ of
+        VersionedDocumentSettingsV1 versionedDocumentSettingsV1 ->
+            encodeSettings versionedDocumentSettingsV1
 
 
-type alias DocV1 =
+type alias Doc =
     { pings : List PingsElements
-    , settings : Settings
+    , settings : DocumentSettings
     }
 
 
-docV1Decoder : Json.Decode.Decoder DocV1
-docV1Decoder =
-    Json.Decode.succeed DocV1
+docDecoder : Json.Decode.Decoder Doc
+docDecoder =
+    Json.Decode.succeed Doc
         |> Json.Decode.Pipeline.required "pings" (Json.Decode.list pingsElementsDecoder)
-        |> Json.Decode.Pipeline.required "settings" settingsDecoder
+        |> Json.Decode.Pipeline.required "settings" documentSettingsDecoder
 
 
-encodeDocV1 : DocV1 -> Json.Encode.Value
-encodeDocV1 docV1_ =
+encodeDoc : Doc -> Json.Encode.Value
+encodeDoc doc_ =
     Json.Encode.object
-        [ ( "pings", Json.Encode.list (/value -> encodePingsElements value) docV1_.pings )
-        , ( "settings", encodeSettings docV1_.settings )
+        [ ( "pings", Json.Encode.list (/value -> encodePingsElements value) doc_.pings )
+        , ( "settings", encodeDocumentSettings doc_.settings )
         , ( "version", Json.Encode.string "v1" )
         ]
 
 
 type DocFromAutomerge
-    = VersionedDocFromAutomergeV1 DocV1
+    = VersionedDocFromAutomergeV1 Doc
 
 
 docFromAutomergeDecoder : Json.Decode.Decoder DocFromAutomerge
@@ -262,7 +262,7 @@ docFromAutomergeDecoder =
         (/tag ->
             case tag of
                 "v1" ->
-                    Json.Decode.map VersionedDocFromAutomergeV1 docV1Decoder
+                    Json.Decode.map VersionedDocFromAutomergeV1 docDecoder
 
                 unknown ->
                     Json.Decode.fail ("Unknown value `" ++ unknown ++ "`")
@@ -274,7 +274,7 @@ encodeDocFromAutomerge : DocFromAutomerge -> Json.Encode.Value
 encodeDocFromAutomerge docFromAutomerge_ =
     case docFromAutomerge_ of
         VersionedDocFromAutomergeV1 versionedDocFromAutomergeV1 ->
-            encodeDocV1 versionedDocFromAutomergeV1
+            encodeDoc versionedDocFromAutomergeV1
 
 
 type GotNewNotificationsPermission
